4f2fdb211bbba3ed3f1c0a43a08962df93a766aa
Victor Savkin <vic.savkin@gmail.com>
2012-11-26 10:00:22 -0500

    implements the parsing of the iterate attribute

--- lib/src/parser/nodes.dart
+++ lib/src/parser/nodes.dart
@@ -114,6 +114,28 @@ class TemplateNode{
   String get ifExpression
     => element.attributes["data-if"];
 
+  bool get hasIfExpression
+    => ifExpression != null;
+
+  String get iterationAttribute
+    => element.attributes["data-iterate"];
+
+  bool get hasIterationAttribute
+    => iterationAttribute != null;
+
+  String get iterationVariable
+    => _splitIterationAttribute[0];
+
+  String get iterationExpression
+    => _splitIterationAttribute[1];
+
   void visit(visitor)
     => visitor.visitTemplate(this);
+
+  get _splitIterationAttribute {
+    var parts = iterationAttribute.split(new RegExp(r"\sin\s"));
+    if(parts.length != 2)
+      throw new DataBinderException("Invalid iteration attribute: '${iterationAttribute}'");
+    return parts;
+  }
 }
--- test/src/parser/nodes_tests.dart
+++ test/src/parser/nodes_tests.dart
@@ -23,5 +23,46 @@ testNodes() {
         expect(n.type, isNull);
       });
     });
+
+    group("TemplateNode", (){
+      Element e;
+
+      setUp((){
+        e = new Element.html("<template data-iterate='item in items'></template>");
+      });
+
+      test("returns iteration variable name", (){
+        var n = new TemplateNode(e);
+        expect(n.iterationVariable, equals("item"));
+      });
+
+      test("returns iteration expression", (){
+        var n = new TemplateNode(e);
+        expect(n.iterationExpression, equals("items"));
+      });
+
+      test("returns true when there is an iteration attribute", (){
+        var n = new TemplateNode(e);
+        expect(n.hasIterationAttribute, isTrue);
+      });
+
+      test("returns false when there no iteration attribute", (){
+        var e = new Element.html("<template></template>");
+        var n = new TemplateNode(e);
+        expect(n.hasIterationAttribute, isFalse);
+      });
+
+      test("iterationExpression throws an exception when there is a malformed iteration attribute", (){
+        var e = new Element.html("<template data-iterate='items'></template>");
+        var n = new TemplateNode(e);
+        expect(() => n.iterationExpression, throwsA(new isInstanceOf<DataBinderException>()));
+      });
+
+      test("iterationVariable throws an exception when there is a malformed iteration attribute", (){
+        var e = new Element.html("<template data-iterate='items'></template>");
+        var n = new TemplateNode(e);
+        expect(() => n.iterationVariable, throwsA(new isInstanceOf<DataBinderException>()));
+      });
+    });
   });
 }
